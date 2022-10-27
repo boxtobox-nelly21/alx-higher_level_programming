18478ccc46d874591fe1e2d30cea6b5469d59271
boxtobox-nelly21 <nellchieng@gmail.com>
2022-10-26 19:16:26 -0700

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:13:37 2022
+
+@author: Nelson Otieno
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from int
+    """
+    def __eq__(self, num):
+        """
+        equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) != num)
+
+    def __ne__(self, num):
+        """
+        no equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) == num)
