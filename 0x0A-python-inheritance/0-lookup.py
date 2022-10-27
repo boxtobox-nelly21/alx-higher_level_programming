5cd37e2348a7133ab263f6b9f997274e7e9bef3a
boxtobox-nelly21 <nellchieng@gmail.com>
2022-10-26 18:19:15 -0700

    Returns objects in a list

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:13:37 2022
+
+@author: Nelson Otieno
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
