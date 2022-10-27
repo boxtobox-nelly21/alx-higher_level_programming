a76b2fe225e9dbe847d7f4a0a7bd27aa1371cc1b
boxtobox-nelly21 <nellchieng@gmail.com>
2022-10-26 19:42:59 -0700

    More Paper work

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 20:19:26 2022
+
+@author: Nelson Otieno
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a json string to a python object
+
+    Arguments:
+        my_obj (str): The inputed object to convert in json format
+
+    Return:
+        A Python object from json
+    """
+    return json.loads(my_str)
