44b592f086d8a8247684e0711b324004e1555fe4
boxtobox-nelly21 <nellchieng@gmail.com>
2022-10-26 18:49:26 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:13:37 2022
+
+@author: Nelson Otieno
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
