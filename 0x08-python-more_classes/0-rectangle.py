253ef077126705250588db45cd50bd5d33abe58e
boxtobox-nelly21 <nellchieng@gmail.com>
2023-10-30 18:27:02 -0700

    Basic Rectangle representations

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 2023
+@author: nelson otieno
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
